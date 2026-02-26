b69d02749aca19f8f88823f727f4a644a54bcae4
Olena <olenahorodn7202@gmail.com>
2026-02-26 20:06:23 +0200

    Change_logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
 
   return (
     <header className="header">
-      <div className="logo">☕ Coffee Shop (develop ) !</div>
+      <div className="logo">☕ Coffee Shop (develop)</div>
       <nav className="nav">
         <Link className={isActive("/") ? "active" : ""} to="/">Головна</Link>
         <Link className={isActive("/catalog") ? "active" : ""} to="/catalog">Каталог</Link>
