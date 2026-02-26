753bf94da7130371b89080bbe54904d139b4368f
Olena <olenahorodn7202@gmail.com>
2026-02-26 19:56:23 +0200

    Change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,12 @@ export default function Header() {
 
   return (
     <header className="header">
-      <div className="logo">Coffee Shop(feature)</div> {/* без іконки */}
+      <div className="logo">Coffee Shop(feature)</div> {/* без іконки */} 
       <nav className="nav">
         <Link className={isActive("/") ? "active" : ""} to="/">Головна</Link>
         <Link className={isActive("/catalog") ? "active" : ""} to="/catalog">Каталог</Link>
         <Link className={isActive("/cart") ? "active" : ""} to="/cart">Кошик</Link>
-      </nav>
+      </nas>
     </header>
   );
 }
